d4eeb6a701d3e959b93d57a1b07f0193b80d9667
Anand Maragur <anand.maragur@gmail.com>
2022-07-13 17:36:27 +0800

    Initial Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,8 @@
 terraform {
-
-
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.7.0"
+      version = "=3.7.0"
     }
   }
   backend "azurerm" {
